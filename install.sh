e85bc02203e7ddc7668aad8c475dcb00d52126d3
Kennedy Kori <kennedykori47@gmail.com>
2022-11-24 11:12:05 +0300

    chore-group-playbook-tasks

--- install.sh
+++ install.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
-# Set "main" as the IDR deploy branch when one isn't provided.
-if [[ ! -v IDR_DEPLOY_BRANCH || ! -n "$IDR_DEPLOY_BRANCH" ]]; then
-    IDR_DEPLOY_BRANCH="main"
-fi
+# Declare and set defaults for supported (env) variables. These variables can be used to change/customize the installation of the application.
+declare -A SupportedVariables
+SupportedVariables[IDR_DEPLOY_BRANCH]=main
+SupportedVariables[FORCE_APP_DOWNLOAD]=no
+
+# Declare each supported variable and set it's default value if the variable wasn't provided at script invocation.
+# i.e The for loop below essentially does the following checks and/or declarations for each variable and default value combination defined in "SupportedVariables".
+# 
+#         # E.g. This will set "main" as the IDR deploy branch when one isn't provided.
+#         if [[ ! -v IDR_DEPLOY_BRANCH || ! -n "$IDR_DEPLOY_BRANCH" ]]; then
+#             IDR_DEPLOY_BRANCH="main"
+#         fi
+
+for key in "${!SupportedVariables[@]}"
+do
+    if [[ ! -v $key || ! -n "${!key}" ]]; then
+        declare "$key"="${SupportedVariables[$key]}"
+    fi
+done
 
 # Source env variables regarding OS details e.g NAME, VERSION_ID
 . /etc/os-release
 
 
 echo "=========================================================================="
-echo "Stats"
+echo "Host and installation parameters"
 echo $'==========================================================================\n'
-echo "HOST OS NAME:      $NAME"
-echo "HOST OS VESRION:   $VERSION"
-echo "IDR DEPLOY BRANCH: $IDR_DEPLOY_BRANCH"
+echo "HOST OS NAME:       $NAME"
+echo "HOST OS VESRION:    $VERSION"
+echo "IDR DEPLOY BRANCH:  $IDR_DEPLOY_BRANCH"
+echo "FORCE APP DOWNLOAD: $FORCE_APP_DOWNLOAD"
 
 
 echo $'\n\n=========================================================================='
@@ -49,9 +65,10 @@ unzip -o /tmp/idr_client_deploy.zip -d /tmp/
 
 # Run the Playbook
 echo $'\n\n=========================================================================='
-echo "Runing the playbook"
+echo "Run the playbook"
 echo $'==========================================================================\n'
-cd /tmp/idr_client_deploy-${IDR_DEPLOY_BRANCH}/playbook && ansible-playbook -i inventory idr_client.yml --ask-vault-pass --ask-become-pass -vvv
-
-exit 0
+cd /tmp/idr_client_deploy-${IDR_DEPLOY_BRANCH}/playbook && ansible-playbook -i inventory idr_client.yml \
+    --ask-vault-pass \
+    --ask-become-pass -vvv \
+    -e "{\"force_app_download\": $(if [[ $FORCE_APP_DOWNLOAD == "yes" ]]; then echo "true"; else echo "false"; fi) }"
 
