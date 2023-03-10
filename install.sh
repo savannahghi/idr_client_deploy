5c92988303bf1379f5ff35161b5ecefb3ccfd4cd
Kennedy Kori <kennedykori47@gmail.com>
2023-03-10 10:25:05 +0300

    chore: migrate to prod
    Change the IDR Server instance used for metadata download and upload to the prod instance.
    
    Included are other minor improvements and fixes and the first implementation of post-installation tasks that perform cleanup and open the config file for editing after a successful installation.

--- install.sh
+++ install.sh
@@ -1,22 +1,35 @@
 #!/usr/bin/env bash
 
-# Declare and set defaults for supported (env) variables. These variables can be used to change/customize the installation of the application.
-declare -A SupportedVariables
-SupportedVariables[IDR_DEPLOY_BRANCH]=main
-SupportedVariables[FORCE_APP_DOWNLOAD]=no
+set -o errexit
+set -o nounset
+set -o pipefail
 
-# Declare each supported variable and set it's default value if the variable wasn't provided at script invocation.
-# i.e The for loop below essentially does the following checks and/or declarations for each variable and default value combination defined in "SupportedVariables".
+# Declare and set defaults for supported (env) variables. These variables can
+# be used to change/customize the installation of the application.
+declare -A DEFAULT_SETTINGS
+DEFAULT_SETTINGS[FORCE_APP_DOWNLOAD]=no
+DEFAULT_SETTINGS[IDR_DEPLOY_BRANCH]=main
+DEFAULT_SETTINGS[LOCAL_BINARY_PATH]=""
+DEFAULT_SETTINGS[SKIP_DESKTOP_ENTRIES]=no
+DEFAULT_SETTINGS[SKIP_POST_INSTALLATION_TASKS]=no
+DEFAULT_SETTINGS[POST_INSTALLATION_SCRIPT_PATH]="/tmp/idr_client_post_installation_tasks.sh"
+
+# Declare each supported variable and set it's default value if the variable
+# wasn't provided at script invocation.
+# i.e The for loop below essentially does the following checks and/or
+# declarations for each variable and default value combination defined in
+# "DEFAULT_SETTINGS".
 # 
-#         # E.g. This will set "main" as the IDR deploy branch when one isn't provided.
+#         # E.g. This will set "main" as the IDR deploy branch when one isn't
+#         # provided.
 #         if [[ ! -v IDR_DEPLOY_BRANCH || ! -n "$IDR_DEPLOY_BRANCH" ]]; then
 #             IDR_DEPLOY_BRANCH="main"
 #         fi
 
-for key in "${!SupportedVariables[@]}"
+for key in "${!DEFAULT_SETTINGS[@]}"
 do
     if [[ ! -v $key || ! -n "${!key}" ]]; then
-        declare "$key"="${SupportedVariables[$key]}"
+        declare "$key"="${DEFAULT_SETTINGS[$key]}"
     fi
 done
 
@@ -29,8 +42,13 @@ echo "Host and installation parameters"
 echo $'==========================================================================\n'
 echo "HOST OS NAME:       $NAME"
 echo "HOST OS VESRION:    $VERSION"
-echo "IDR DEPLOY BRANCH:  $IDR_DEPLOY_BRANCH"
-echo "FORCE APP DOWNLOAD: $FORCE_APP_DOWNLOAD"
+echo ""
+echo "FORCE APP DOWNLOAD:               $FORCE_APP_DOWNLOAD"
+echo "IDR DEPLOY BRANCH:                $IDR_DEPLOY_BRANCH"
+echo "LOCAL BINARY PATH:                $LOCAL_BINARY_PATH"
+echo "SKIP DESKTOP ENTRIES:             $SKIP_DESKTOP_ENTRIES"
+echo "SKIP POST INSTALLATION TASKS:     $SKIP_POST_INSTALLATION_TASKS"
+echo "POST INSTALLATION SCRIPT PATH:    $POST_INSTALLATION_SCRIPT_PATH" 
 
 
 echo $'\n\n=========================================================================='
@@ -46,7 +64,7 @@ elif [[ $VERSION_ID == "18.04" ]]
 then
     add-apt-repository --yes --update ppa:ansible/ansible-2.9
 fi
-apt install ansible cron desktop-file-utils unzip -y
+apt install ansible unzip -y
 
 
 # Download the IDR Client Installation Playbook Archive
@@ -70,5 +88,20 @@ echo $'=========================================================================
 cd /tmp/idr_client_deploy-${IDR_DEPLOY_BRANCH}/playbook && ansible-playbook -i inventory idr_client.yml \
     --ask-vault-pass \
     --ask-become-pass -vvv \
-    -e "{\"force_app_download\": $(if [[ $FORCE_APP_DOWNLOAD == "yes" ]]; then echo "true"; else echo "false"; fi) }"
+    -e "{\"do_add_desktop_entries\": $(if [[ $SKIP_DESKTOP_ENTRIES != "yes" ]]; then echo "true"; else echo "false"; fi) }" \
+    -e "{\"do_execute_post_installation_tasks\": $(if [[ $SKIP_POST_INSTALLATION_TASKS != "yes" ]]; then echo "true"; else echo "false"; fi) }" \
+    -e "{\"force_app_download\": $(if [[ $FORCE_APP_DOWNLOAD == "yes" ]]; then echo "true"; else echo "false"; fi) }" \
+    -e "{\"local_binary_path\": \"$LOCAL_BINARY_PATH\" }" \
+    -e "{\"post_installation_script_path\": \"$POST_INSTALLATION_SCRIPT_PATH\" }"
 
+
+if [[ $SKIP_POST_INSTALLATION_TASKS != "yes" ]]
+then
+    # Edit the configuration file
+    echo $'\n\n=========================================================================='
+    echo "Edit the config file with host specific settings"
+    echo $'==========================================================================\n'
+    echo "Editing client config file ..."
+    . $POST_INSTALLATION_SCRIPT_PATH
+    echo "Done"
+fi
